43d2652a4f435982c0926feb1f9990cad7c8e6c2
Adrian Tello <adriantl7@gmail.com>
2016-12-18 12:39:00 +0100

    Add a message to wrap the generated number

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const APP_ID = 'amzn1.ask.skill.7da93716-df16-4cbf-9b92-32a35246e4e8';
 const generateNumber = function(){
     const myNumber = Math.floor((Math.random() * MAX_NUMER) + 1);
 
-    this.attributes.speechOutput = myNumber;
-    this.emit(':tell', myNumber);
+    this.attributes.speechOutput = this.t('NUMBER_MESSAGE', myNumber);
+    this.emit(':tell', this.attributes.speechOutput);
 };
 
 const handlers = {
@@ -46,6 +46,7 @@ const languageStrings = {
     'en-GB': {
         translation: {
             SKILL_NAME: 'Random number generator',
+            NUMBER_MESSAGE: 'The generated number is: %d',
             HELP_MESSAGE: "You can generate numbers saying, generate random number, or, you can say exit...Now, what can I help you with?",
             HELP_REPROMT: "You can say things like, generate random number, or you can say exit...Now, what can I help you with?",
             STOP_MESSAGE: 'Goodbye!'
@@ -54,6 +55,7 @@ const languageStrings = {
     'en-US': {
         translation: {
             SKILL_NAME: 'Random number generator',
+            NUMBER_MESSAGE: 'The generated number is: %d',
             HELP_MESSAGE: "You can generate numbers saying, generate random number, or, you can say exit...Now, what can I help you with?",
             HELP_REPROMT: "You can say things like, generate random number, or you can say exit...Now, what can I help you with?",
             STOP_MESSAGE: 'Goodbye!'
@@ -62,6 +64,7 @@ const languageStrings = {
     'de-DE': {
         translation: {
             SKILL_NAME: 'Zufallszahlengenerator',
+            NUMBER_MESSAGE: 'Die generierte Zahl lautet: %d',
             HELP_MESSAGE: 'Du kannst beispielsweise Fragen sagen wie „Zufallszahl generieren“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?',
             HELP_REPROMT: 'Du kannst beispielsweise Sachen sagen wie „Zufallszahl generieren“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?',
             STOP_MESSAGE: 'Auf Wiedersehen!'
